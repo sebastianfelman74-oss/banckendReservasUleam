c594d39a40d4b52ea6acdacea3a180ccfd813f4e
sebastianfelman74-oss <sebastianfelman74@gmail.com>
2026-04-27 21:56:50 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,6 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
-});
+const connection = mysql.createConnection(process.env.MYSQL_URL);
 
 connection.connect(err => {
     if (err) {
